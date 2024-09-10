756104fdbe7f52d9456a5e623707543ed700b60b
Bruno Andrade <brunoandradejp@gmail.com>
2024-09-10 11:04:16 -0300

    Add follower operations and DTOs
    Implemented follower management by adding methods to check and persist followers, and endpoint to list followers. Introduced DTOs for follower responses and overall follower counts, enhancing the API's data handling and response structure.

--- src/main/java/com/brunoandrade/quarkussocial/domain/repository/FollowerRepository.java
+++ src/main/java/com/brunoandrade/quarkussocial/domain/repository/FollowerRepository.java
@@ -1,9 +1,32 @@
 package com.brunoandrade.quarkussocial.domain.repository;
 
 import com.brunoandrade.quarkussocial.domain.model.Follower;
+import com.brunoandrade.quarkussocial.domain.model.User;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import io.quarkus.panache.common.Parameters;
 import jakarta.enterprise.context.ApplicationScoped;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @ApplicationScoped
 public class FollowerRepository implements PanacheRepository<Follower> {
+
+    public boolean follows(User follower, User user){
+        Map<String, Object> params = Parameters
+                .with("follower", follower)
+                .and("user", user).map();
+         PanacheQuery<Follower> query = find ("follower =:follower and user =:user", params);
+         Optional<Follower> result = query.firstResultOptional();
+
+         return result.isPresent();
+    }
+
+    public List<Follower> findByUser(Long userId){
+        PanacheQuery<Follower>query = find("user.id", userId);
+        return query.list();
+    }
 }
--- src/main/java/com/brunoandrade/quarkussocial/rest/FollowerResource.java
+++ src/main/java/com/brunoandrade/quarkussocial/rest/FollowerResource.java
@@ -5,10 +5,16 @@ import com.brunoandrade.quarkussocial.domain.model.User;
 import com.brunoandrade.quarkussocial.domain.repository.FollowerRepository;
 import com.brunoandrade.quarkussocial.domain.repository.UserRepository;
 import com.brunoandrade.quarkussocial.rest.dto.FollowerRequest;
+import com.brunoandrade.quarkussocial.rest.dto.FollowerResponse;
+import com.brunoandrade.quarkussocial.rest.dto.FollowersPerUserResponse;
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Path("/users/{userId}/followers")
 @Consumes(MediaType.APPLICATION_JSON)
@@ -25,25 +31,55 @@ public class FollowerResource {
     }
 
     @PUT
-    public Response followers(
+    @Transactional
+    public Response followersUser(
             @PathParam("userId") Long userId,
             FollowerRequest followerRequest
-    ){
-       User user = userRepository.findById(userId);
-       if(user == null) {
-           return Response.status(Response.Status.NOT_FOUND).build();
-       }
+    ) {
+        if (userId.equals(followerRequest.getFollowerId())) {
+            return Response.status(Response.Status.CONFLICT).entity("You can't follow yourself").build();
+        }
 
-       User follower = userRepository.findById(followerRequest.getFollowerId());
+        User user = userRepository.findById(userId);
 
-       var entity = new Follower();
-       entity.setUser(user);
-       entity.setFollower(follower);
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
 
-       followerRepository.persist(entity);
-       return Response.status(Response.Status.NO_CONTENT).build();
+        User follower = userRepository.findById(followerRequest.getFollowerId());
 
+        boolean follows = followerRepository.follows(follower, user);
 
+        if (!follows) {
+            var entity = new Follower();
+            entity.setUser(user);
+            entity.setFollower(follower);
+            followerRepository.persist(entity);
+        }
+
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
+
+    @GET
+    public Response listFollowers(@PathParam("userId") Long userId) {
+
+        User user = userRepository.findById(userId);
+
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        List<Follower> list = followerRepository.findByUser(userId);
+        FollowersPerUserResponse responseObject = new FollowersPerUserResponse();
+        responseObject.setFollowersCount(list.size());
+
+        List<FollowerResponse> follwersList = list
+                .stream()
+                .map(FollowerResponse::new)
+                .toList();
+
+        responseObject.setContent(follwersList);
+        return Response.ok(responseObject).build();
 
 
     }
