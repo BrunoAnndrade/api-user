5682bedec03693bb25a243a13cedd2e7674fdd74
Bruno Andrade <brunoandradejp@gmail.com>
2024-09-11 11:30:22 -0300

    Update UserResourceTest and fix POM system properties
    Refactored test cases in UserResourceTest to include URL injection and method ordering, and added a new test for listing users. Updated POM file to use the correct maven.compiler.release property instead of maven.home.

--- src/test/java/com/brunoandrade/quarkussocial/rest/UserResourceTest.java
+++ src/test/java/com/brunoandrade/quarkussocial/rest/UserResourceTest.java
@@ -2,20 +2,27 @@ package com.brunoandrade.quarkussocial.rest;
 
 import com.brunoandrade.quarkussocial.rest.dto.CreateUserRequest;
 import com.brunoandrade.quarkussocial.rest.dto.ResponseError;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.*;
 
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserResourceTest {
+
+    @TestHTTPResource("/users")
+    URL apiURL;
 
     @Test
     @DisplayName("Should create an user successfully")
+    @Order(1)
     public void createUserTest() {
         var user = new CreateUserRequest();
         user.setName("Fulano");
@@ -26,7 +33,7 @@ class UserResourceTest {
                         .contentType(ContentType.JSON)
                         .body(user)
                .when()
-                        .post("/users")
+                        .post(apiURL)
                .then()
                         .extract().response();
 
@@ -36,6 +43,7 @@ class UserResourceTest {
 
     @Test
     @DisplayName("Should return error when json is not valid")
+    @Order(2)
     public void createUserWithInvalidDataTest() {
         var user = new CreateUserRequest();
         user.setAge(null);
@@ -46,7 +54,7 @@ class UserResourceTest {
                         .contentType(ContentType.JSON)
                         .body(user)
                .when()
-                        .post("/users")
+                        .post(apiURL)
                .then()
                         .extract().response();
 
@@ -56,7 +64,20 @@ class UserResourceTest {
         List<Map<String,String>> errors = response.jsonPath().getList("fieldErrors");
         assertNotNull(errors.get(0).get("message"));
         assertNotNull(errors.get(1).get("message"));
+    }
 
+    @Test
+    @DisplayName("Should list all users")
+    @Order(3)
+    public void listAllUsersTest() {
+        var response =
+               given()
+                        .contentType(ContentType.JSON)
+               .when()
+                        .get(apiURL)
+               .then()
+                       .statusCode(200)
+                       .body("size()", Matchers.is(1));
 
     }
 }
