fd60339d9239bc02ef9469be6c113cf8698ef0e2
Bruno Andrade <brunoandradejp@gmail.com>
2024-09-13 15:59:41 -0300

    Add test for listing a user's followers.
    Introduces a new test case to ensure the endpoint for listing a user's followers functions as expected. Also includes necessary setup changes such as injecting the `FollowerRepository` and creating a follower in the setup method.

--- src/test/java/com/brunoandrade/quarkussocial/rest/FollowerResourceTest.java
+++ src/test/java/com/brunoandrade/quarkussocial/rest/FollowerResourceTest.java
@@ -1,6 +1,8 @@
 package com.brunoandrade.quarkussocial.rest;
 
+import com.brunoandrade.quarkussocial.domain.model.Follower;
 import com.brunoandrade.quarkussocial.domain.model.User;
+import com.brunoandrade.quarkussocial.domain.repository.FollowerRepository;
 import com.brunoandrade.quarkussocial.domain.repository.UserRepository;
 import com.brunoandrade.quarkussocial.rest.dto.FollowerRequest;
 import io.quarkus.test.common.http.TestHTTPEndpoint;
@@ -10,10 +12,9 @@ import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
 import jakarta.ws.rs.core.Response;
 import org.hamcrest.Matchers;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @QuarkusTest
 @TestHTTPEndpoint(FollowerResource.class)
@@ -22,12 +23,15 @@ class FollowerResourceTest {
     @Inject
     UserRepository userRepository;
 
+    @Inject
+    FollowerRepository followerRepository;
+
     Long userId;
     Long userFollowerId;
 
     @BeforeEach
     @Transactional
-    void setUp(){
+    void setUp() {
         //Standard test user
         var user = new User();
         user.setName("Fulano");
@@ -35,17 +39,22 @@ class FollowerResourceTest {
         userRepository.persist(user);
         userId = user.getId();
 
-        //Follower
         var userFollower = new User();
         userFollower.setName("Ciclano");
         userFollower.setAge(35);
         userRepository.persist(userFollower);
         userFollowerId = userFollower.getId();
+
+        //create a follower
+        Follower follower = new Follower();
+        follower.setUser(user);
+        follower.setFollower(userFollower);
+        followerRepository.persist(follower);
     }
 
     @Test
     @DisplayName("Should return 409 when followerId is equal to User id")
-    public void sameUserAsFollowerTest(){
+    public void sameUserAsFollowerTest() {
 
         var body = new FollowerRequest();
         body.setFollowerId(userId);
@@ -54,16 +63,16 @@ class FollowerResourceTest {
                 .contentType(ContentType.JSON)
                 .body(body)
                 .pathParam("userId", userId)
-       .when()
+                .when()
                 .put()
-        .then()
+                .then()
                 .statusCode(Response.Status.CONFLICT.getStatusCode())
                 .body(Matchers.is("You can't follow yourself"));
     }
 
     @Test
     @DisplayName("Should return 404 on follow a user when userId doesn't exist")
-    public void userNotFoundWhenTryingToFollowTest(){
+    public void userNotFoundWhenTryingToFollowTest() {
 
         var nonExistentUserId = 999;
         var body = new FollowerRequest();
@@ -73,15 +82,15 @@ class FollowerResourceTest {
                 .contentType(ContentType.JSON)
                 .body(body)
                 .pathParam("userId", nonExistentUserId)
-        .when()
+                .when()
                 .put()
-        .then()
+                .then()
                 .statusCode(Response.Status.NOT_FOUND.getStatusCode());
     }
 
     @Test
     @DisplayName("Should follow a user")
-    public void followUserTest(){
+    public void followUserTest() {
 
         var body = new FollowerRequest();
         body.setFollowerId(userFollowerId);
@@ -90,24 +99,43 @@ class FollowerResourceTest {
                 .contentType(ContentType.JSON)
                 .body(body)
                 .pathParam("userId", userId)
-        .when()
+                .when()
                 .put()
-        .then()
+                .then()
                 .statusCode(Response.Status.NO_CONTENT.getStatusCode());
     }
 
     @Test
     @DisplayName("Should return 404 on list user followers and userId doesn't exist")
-    public void userNotFoundWhenListingFollowersTest(){
+    public void userNotFoundWhenListingFollowersTest() {
 
         var nonExistentUserId = 999;
 
         given()
                 .contentType(ContentType.JSON)
                 .pathParam("userId", nonExistentUserId)
-        .when()
+                .when()
                 .get()
-        .then()
+                .then()
                 .statusCode(Response.Status.NOT_FOUND.getStatusCode());
+    }
+
+    @Test
+    @DisplayName("Should list a user's followers")
+    public void listFollowersTest() {
+        var response =
+                given()
+                        .contentType(ContentType.JSON)
+                        .pathParam("userId", userId)
+                .when()
+                        .get()
+                .then()
+                        .extract().response();
+
+        var followersCount = response.jsonPath().get("followersCount");
+        var followersContent = response.jsonPath().getList("content");
+        assertEquals(Response.Status.OK.getStatusCode(), response.statusCode());
+        assertEquals(1, followersCount);
+        assertEquals(1, followersContent.size());
     }
 }
